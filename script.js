f49ff219b2c3ede3a683f87bf8bb9a9cbea1b772
HeitorDalla <heitorvillavilla@gmail.com>
2025-03-28 00:00:44 -0300

    Codigo do js comecado

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 "use strict";
 
+const alternativasCorretas = {
+    
+}
+
+const todasPerguntas = [...document.querySelectorAll(".pergunta")];
+
+todasPerguntas.forEach((pergunta) => {
+    console.log("oi")
+});
+
+
+
+
+// document.querySelector("#iniciarQuiz").addEventListener("click", (event) => {
+//     event.preventDefault();
+
+
+
+// })
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     event.preventDefault();
+
+
+
+// })
